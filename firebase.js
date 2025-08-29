3e823debc2a09aa6df5fa5ada61b1bacaf5afee5
itsareebaah <areebaahmadzps@gmail.com>
2025-08-29 23:06:10 +0500

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration object
 const firebaseConfig = {
-    apiKey: "AIzaSyDhWEXCaFai0LJ6kHygouW9o5x4AsGbChI",
-    authDomain: "areebalearnera.firebaseapp.com",
-    projectId: "areebalearnera",
-    storageBucket: "areebalearnera.firebasestorage.app",
-    messagingSenderId: "795374024151",
-    appId: "1:795374024151:web:3598e41aa1bf3f59e70792"
+     apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
   };
 
 
